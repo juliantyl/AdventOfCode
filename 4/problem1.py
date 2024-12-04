f3fcb6abba70a07ed1358629b0e5bbef62bb8011
juliantyl <julian.tyl@hotmail.com>
2024-12-04 16:45:44 +1100

    day 4

--- 4/problem1.py
+++ 4/problem1.py
@@ -10,3 +10,50 @@ filename = "input.txt"
 
 input = read_input_from_file(filename)
 lines = input_lines(input)
+
+# 140x140
+lines = lines[:140]
+print(len(lines))
+out = 0
+for i in range(len(lines)):
+    for j in range(len(lines[i])):
+        if lines[i][j] == "X":
+            # LEFT
+            if i >= 3:
+                if lines[i - 1][j] + lines[i-2][j] + lines[i-3][j] == 'MAS':
+                    out += 1
+                    print('left')
+                if j >= 3:
+                    if lines[i - 1][j - 1] + lines[i - 2][j - 2] + lines[i - 3][j - 3] == 'MAS':
+                        out += 1
+                        print('left up')
+                if j < len(lines[i]) - 3:
+                    if lines[i - 1][j + 1] + lines[i - 2][j + 2] + lines[i - 3][j + 3] == 'MAS':
+                        out += 1
+                        print('left down')
+            # RIGHT
+            if i < len(lines) - 3:
+                if lines[i + 1][j] + lines[i + 2][j] + lines[i + 3][j] == 'MAS':
+                    out += 1
+                    print('right')
+                if j >= 3:
+                    if lines[i + 1][j - 1] + lines[i + 2][j - 2] + lines[i + 3][j - 3] == 'MAS':
+                        out += 1
+                        print('right up')
+                if j < len(lines[i]) - 3:
+                    if lines[i + 1][j + 1] + lines[i + 2][j + 2] + lines[i + 3][j + 3] == 'MAS':
+                        out += 1
+                        print('right down')
+            # UP
+            if j >= 3:
+                if lines[i][j - 1] + lines[i][j - 2] + lines[i][j - 3] == 'MAS':
+                    out += 1
+                    print('up')
+            # DOWN
+            if j < len(lines) - 3:
+                if lines[i][j + 1] + lines[i][j + 2] + lines[i][j + 3] == 'MAS':
+                    out += 1
+                    print('down')
+
+
+print(out)
--- 4/problem2.py
+++ 4/problem2.py
@@ -10,3 +10,32 @@ filename = "input.txt"
 
 input = read_input_from_file(filename)
 lines = input_lines(input)
+
+# 140x140
+lines = lines[:140]
+print(len(lines))
+out = 0
+for i in range(len(lines)):
+    for j in range(len(lines[i])):
+        if lines[i][j] == "A":
+
+            if i >= 1 and i < len(lines) - 1 and j >= 1 and j < len(lines[i]) - 1:
+                # check verts
+                string_vert = lines[i - 1][j] + lines[i + 1][j] + lines[i][j + 1] + lines[i][j - 1]
+                vert_count = Counter(string_vert)
+                if vert_count['M'] == 2 and vert_count['S'] == 2 and string_vert[0] != string_vert[1]:
+                    print(f'VERT At [{i}, {j}]')
+                    print(f'.{string_vert[0]}.')
+                    print(f'{string_vert[3]}A{string_vert[2]}.')
+                    print(f'.{string_vert[1]}.')
+                    # out += 1
+                # check diags
+                string_diag = lines[i - 1][j - 1] + lines[i + 1][j + 1] + lines[i - 1][j + 1] + lines[i + 1][j - 1]
+                diag_count = Counter(string_diag)
+                if diag_count['M'] == 2 and diag_count['S'] == 2 and string_diag[0] != string_diag[1]:
+                    print(f'DIAG At [{i}, {j}]')
+                    print(f'{string_diag[0]}.{string_diag[2]}')
+                    print(f'.A.')
+                    print(f'{string_diag[3]}.{string_diag[1]}')
+                    out += 1
+print(out)
