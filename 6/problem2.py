74ea4451c3ebdf2ebe898469d58ae689dc16121d
juliantyl <julian.tyl@hotmail.com>
2024-12-08 13:22:34 +1100

    finally day 6

--- 6/problem2.py
+++ 6/problem2.py
@@ -7,19 +7,17 @@ from itertools import cycle, combinations, accumulate, chain
 from aoc.helpers import output, read_input_from_file, input_lines
 
 
-filename = "test.txt"
+filename = "input.txt"
 
 input = read_input_from_file(filename)
 lines = input_lines(input)
 
-visited = []
 blocks = []
 
 for line in lines:
     if line == '':
         lines.remove(line)
     else:
-        visited.append([list() for i in range(len(line))])
         blocks.append(len(line) * [0])
 
 pos = (0, 0)
@@ -30,10 +28,9 @@ for i in range(len(lines)):
         if lines[i][j] == '>' or lines[i][j] == '<' or lines[i][j] == '^' or lines[i][j] == 'v':
             pos = (i, j)
             direc = lines[i][j]
-            visited[i][j].append(direc)
-            blocks[i][j] = -1
+            blocks[i][j] = 'X'
         elif lines[i][j] == '#':
-            blocks[i][j] = -1
+            blocks[i][j] = 'X'
 
 
 left = False
@@ -51,52 +48,38 @@ def get_next_dir(direction):
         return ""
 
 def check_if_blocc(position, direction, blocc_pos):
-    # if we keep walking, will we reach wall or edge first (FALSE) or same dir first (TRUE)
-    sim_pos = (position[0], position[1])
+    sim_pos = position
     sim_dir = direction
-    sim_visited = copy.deepcopy(visited)
+    visited_positions = set()
 
     while True:
         next_tile = (sim_pos[0] + dirs[sim_dir][0], sim_pos[1] + dirs[sim_dir][1])
         if next_tile[0] < 0 or next_tile[0] >= len(lines) or next_tile[1] < 0 or next_tile[1] >= len(lines[0]):
             return False
-        elif sim_dir in sim_visited[next_tile[0]][next_tile[1]]:
+        if (next_tile, sim_dir) in visited_positions:
             return True
-        elif lines[next_tile[0]][next_tile[1]] == '#' or (next_tile[0] == blocc_pos[0] and next_tile[1] == blocc_pos[1]):
-            # return False
+        visited_positions.add((next_tile, sim_dir))
+        if lines[next_tile[0]][next_tile[1]] == '#' or next_tile == blocc_pos:
             sim_dir = get_next_dir(sim_dir)
         else:
-            sim_pos = (next_tile[0], next_tile[1])
-            sim_visited[next_tile[0]][next_tile[1]].append(sim_dir)
-            # print(sim_dir)
+            sim_pos = next_tile
+
 
 out = 0
 while not left:
-
     next_tile = (pos[0] + dirs[direc][0], pos[1] + dirs[direc][1])
-    # print(f'NEXT TILE: {next_tile}')
     if next_tile[0] < 0 or next_tile[0] >= len(lines) or next_tile[1] < 0 or next_tile[1] >= len(lines[0]):
-        # print(f"DONE")
         left = True
     elif lines[next_tile[0]][next_tile[1]] == '#':
-        # print("HIT A WALL, TURNING")
         direc = get_next_dir(direc)
-        visited[pos[0]][pos[1]].append(direc)
 
-    elif check_if_blocc(pos, get_next_dir(direc), next_tile):
-        if blocks[next_tile[0]][next_tile[1]] == 0:
-            blocks[next_tile[0]][next_tile[1]] = 1
-            print(f'placeable at {next_tile[0]}, {next_tile[1]}')
-            out += 1
+    elif blocks[next_tile[0]][next_tile[1]] == 0 and check_if_blocc(pos, get_next_dir(direc), next_tile):
+        blocks[next_tile[0]][next_tile[1]] = 1
+        print(f'placeable at {next_tile[0]}, {next_tile[1]}')
+        out += 1
         pos = (next_tile[0], next_tile[1])
-        # print(visited[next_tile[0]][next_tile[1]])
-        visited[next_tile[0]][next_tile[1]].append(direc)
-        # print(visited[next_tile[0]][next_tile[1]])
     else:
-        # print("CONTINUING B")
+        blocks[next_tile[0]][next_tile[1]] = 1
         pos = (next_tile[0], next_tile[1])
-        # print(visited[next_tile[0]][next_tile[1]])
-        visited[next_tile[0]][next_tile[1]].append(direc)
-        # print(visited[next_tile[0]][next_tile[1]])
 
 print(out)
